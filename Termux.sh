14e8740316c1dfa5183c5fea90ac34d46511b67a
Ryuk <88324835+anonymousx97@users.noreply.github.com>
2023-02-21 19:04:53 +0530

    Update Termux.sh

--- Termux.sh
+++ Termux.sh
@@ -1,9 +1,3 @@
-outt() {
-    { "$@" || return $?; } | while read -r line; do
-        :
-    done
-}
-
 welcome(){
 echo -e "
 The process will take 200-300mb to download files
@@ -11,44 +5,43 @@ and will take around 900mb of storage after unpack.
 "
 }
 
-update(){
-echo -e "1.Updating packages"
-yes|outt apt update
-yes|outt apt dist-upgrade
-echo -e "- Done"
-}
+string(){
+if [ $HOME == "/data/data/com.termux/files/home" ]; then
+  if [ $TERMUX_APK_RELEASE == "F_DROID" ]; then
+    echo -e "1.Updating packages"
+    echo -e "Change default termux mirror to Single North America.
+    \nEntering Termux mirror menu in 10 sec."
+    sleep 10
+    termux-change-repo
+    yes|pkg upgrade 2> /dev/null /dev/null
+    apt -qq update -y 2> /dev/null /dev/null
+    apt -qq upgrade -y 2> /dev/null /dev/null
+    echo -e "- Done"
+    echo -e "Installing Python"
+    apt -qq install -y git python python-pip 2> /dev/null /dev/null
+    pip install -q -U setuptools wheel tgcrypto
+    echo -e "- Done"
+  else
+    echo -e "Termux from Playstore not supported.\n(⁠ノ⁠ಠ⁠益⁠ಠ⁠)⁠ノ"
+    exit
+  fi
+else
+  echo -e "Not a termux env
+  \nInstall python, pip, git and then run command again if next part fails."
+fi
 
-install(){
-echo -e "2.Installing Requirements,
-  This may take upto 5 minutes or more, depending 
-  on your internet speed and device capabilities."
-
-outt "$(
-apt install -y git wget python
-pip install python-dotenv
-pip install wheel
-pip install tgcrypto
-pip install poetry-core==1.0.7
-pip install --upgrade pip)"
-echo -e "- Done"
-}
-
-clone(){
 echo -e "3.Clonning String Repo"
-outt rm -rf string
-outt git clone -q https://github.com/ux-termux/string string
+if [ -d "string" ]; then 
+  echo -e "A folder named string exists.\nRun command again after deleting it"
+  exit
+fi
+git clone -q https://github.com/ux-termux/string string
 echo -e "- Done"
-}
 
-start(){
 echo -e "4.Starting Generator"
 cd string
-sleep 2
 bash gen
 }
 
 welcome
-update 2> /dev/null
-install 2> /dev/null
-clone
-start
+string
