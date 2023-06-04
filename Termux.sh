c451d9e18e5d612b1f10f6abdb717413e2a28714
anonymousx97 <88324835+anonymousx97@users.noreply.github>
2023-06-04 16:20:16 +0530

    Potential Server Support.

--- Termux.sh
+++ Termux.sh
@@ -1,66 +1,73 @@
 #!/bin/bash
 
 welcome(){
-echo -e "
-The package installation process will take 200-300mb to download files
-and will take around 900mb of storage after unpack.
-"
+    if [ $HOME == "/data/data/com.termux/files/home" ]; then
+        if ! [ $TERMUX_APK_RELEASE == "F_DROID" ]; then
+            echo -e "Termux from Playstore not supported.\n(⁠ノ⁠ಠ⁠益⁠ಠ⁠)⁠ノ"
+            exit
+        fi
+        termux_env=true
+        echo -e "The package installation process will take 200-300mb to download files and will take around 900mb of storage after unpack."
+    else
+        echo -e "Not a termux env
+Install python, pip if the script is unable to install in the next step."
+    fi
 }
 
 update(){
-clear
-if [ $HOME == "/data/data/com.termux/files/home" ]; then
-  if [ $TERMUX_APK_RELEASE == "F_DROID" ]; then
-    echo -e "1.Updating packages"
+    clear
+    echo "1.Updating packages"
     echo -e "ATTENTION\nChange default termux mirror to Single North America."
     read -p "Press Enter to proceed
 >"
     termux-change-repo
     yes|pkg upgrade
     apt update -y
-    apt upgrade -y
-    echo -e "- Done"
-  else
-    echo -e "Termux from Playstore not supported.\n(⁠ノ⁠ಠ⁠益⁠ಠ⁠)⁠ノ"
-    exit
-  fi
-else
-  echo -e "Not a termux env
-  \nInstall python, pip, git and then run command again if next part fails."
-fi
+    clear
+    echo -e "- Done."
 }
 
 install_py(){
-clear
-echo -e "2. Installing Python"
-apt install -y python python-pip
-pip install -q -U setuptools wheel tgcrypto
-echo -e "- Done"
+    echo -e "2. Installing Python"
+    if [ $termux_env ] ; then
+        apt install -y python python-pip
+    else
+        apt install -y python3 python3-pip
+    fi
+    echo -e "Installing Pypi packages."
+    pip install -q -U setuptools wheel tgcrypto
+    echo -e "- Done"
 }
 
 start_gen(){
-clear
-echo -e "4.Starting Generator"
-python -c "$(curl -fsSL https://raw.githubusercontent.com/ux-termux/string/main/gen.py)"
+    clear
+    echo -e "4. Fetching Generator script."
+    python -c "$(curl -fsSL https://raw.githubusercontent.com/ux-termux/string/main/gen.py)"
+}
+
+options(){
+    echo -e "Select an Option :
+1. Update packages ( For termux )
+2. Install Python and PIP
+3. Do Both and Start Generator ( Recommended )
+4. Skip Both and Start Generator
+5. Exit"
 }
 
 clear
 welcome
+options
+
 while true; do
-    echo -e "Select an Option:
-1. Update packages ( For termux )
-2. Install Python and PIP
-3. Do both and start Generator ( Recommended )
-4. Skip Both and Start Generator
-  ( Only skip if your termux installation is not fresh and Packages are installed already. )
-5. Exit"
     read -p "> " inp
     if [ -z $inp ]; then
-        pass
+        :
     elif [ $inp == "1" ]; then
         update
+        options
     elif [ $inp == "2" ]; then
         install_py
+        options
     elif [ $inp == "3" ]; then
         update
         install_py
