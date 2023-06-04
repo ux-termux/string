4295016eb467fd87a5f4b65aa3fc85c899ecdefd
anonymousx97 <88324835+anonymousx97@users.noreply.github>
2023-06-04 15:22:50 +0530

    switch to curling scripts instead of clone.

--- Termux.sh
+++ Termux.sh
@@ -1,21 +1,24 @@
+#!/bin/bash
+
 welcome(){
 echo -e "
-The process will take 200-300mb to download files
+The package installation process will take 200-300mb to download files
 and will take around 900mb of storage after unpack.
 "
 }
 
-string(){
+update(){
+clear
 if [ $HOME == "/data/data/com.termux/files/home" ]; then
   if [ $TERMUX_APK_RELEASE == "F_DROID" ]; then
     echo -e "1.Updating packages"
-    echo -e "    ATTENTION\nChange default termux mirror to Single North America."
+    echo -e "ATTENTION\nChange default termux mirror to Single North America."
     read -p "Press Enter to proceed
-    >"
+>"
     termux-change-repo
-    yes|pkg upgrade 2> /dev/null /dev/null
-    apt -qq update -y 2> /dev/null /dev/null
-    apt -qq upgrade -y 2> /dev/null /dev/null
+    yes|pkg upgrade
+    apt update -y
+    apt upgrade -y
     echo -e "- Done"
   else
     echo -e "Termux from Playstore not supported.\n(⁠ノ⁠ಠ⁠益⁠ಠ⁠)⁠ノ"
@@ -25,24 +28,50 @@ else
   echo -e "Not a termux env
   \nInstall python, pip, git and then run command again if next part fails."
 fi
-
-echo -e "Installing Python"
-apt install -y git python python-pip
-pip install -q -U setuptools wheel tgcrypto
-echo -e "- Done"
-
-echo -e "3.Clonning String Repo"
-if [ -d "string" ]; then 
-  echo -e "A folder named string exists.\nRun command again after deleting it"
-  exit
-fi
-git clone -q https://github.com/ux-termux/string string
-echo -e "- Done"
-
-echo -e "4.Starting Generator"
-cd string
-bash gen
 }
 
+install_py(){
+clear
+echo -e "2. Installing Python"
+apt install -y python python-pip
+pip install -q -U setuptools wheel tgcrypto
+echo -e "- Done"
+}
+
+start_gen(){
+clear
+echo -e "4.Starting Generator"
+python -c "$(curl -fsSL https://raw.githubusercontent.com/ux-termux/string/main/gen.py)"
+}
+
+clear
 welcome
-string
+while true; do
+    echo -e "Select an Option:
+1. Update packages ( For termux )
+2. Install Python and PIP
+3. Do both and start Generator ( Recommended )
+4. Skip Both and Start Generator
+  ( Only skip if your termux installation is not fresh and Packages are installed already. )
+5. Exit"
+    read -p "> " inp
+    if [ -z $inp ]; then
+        pass
+    elif [ $inp == "1" ]; then
+        update
+    elif [ $inp == "2" ]; then
+        install_py
+    elif [ $inp == "3" ]; then
+        update
+        install_py
+        start_gen
+        exit 
+    elif [ $inp == "4" ]; then 
+        start_gen
+        exit 
+    elif [ $inp == "5" ];then
+        exit
+    else
+        echo "Wrong Input."
+    fi
+done
--- gen.py
+++ gen.py
@@ -1,22 +1,36 @@
-import os
-from pyrogram import Client
-from pyrogram import __version__
+import subprocess
 
-if os.path.isfile("string.session"): os.remove("string.session")
+print(
+    """Enter 1 if generating for Userge-X bot.
+Enter 2 for Pyrogram V2.
+*** DO NOT enter 2 if generating for Userge-X. ***
+"""
+)
+
+while True:
+    inp = input("> ")
+    if inp in [1, "1"]:
+        subprocess.call("pip install pyrogram==1.4.7", shell=True)
+        break
+    elif inp in [2, "2"]:
+        subprocess.call("pip install pyrogram==2.0.57", shell=True)
+        break
+    else:
+        print("Wrong Input\nTry Again.")
+
+subprocess.call("clear",shell=True)
+
+from pyrogram import Client, __version__
 
 API_ID = int(input("Enter API_ID: "))
 API_HASH = input("Enter API_HASH: ")
-pyro_ver = __version__
 
-if pyro_ver == "1.4.7":
-    client = Client(session_name="string",api_id=API_ID, api_hash=API_HASH)
+if __version__ == "1.4.7":
+    client = Client(session_name=":memory:", api_id=API_ID, api_hash=API_HASH)
 else:
-    client = Client(name='string', api_id=API_ID, api_hash=API_HASH, in_memory=True)
+    client = Client(name="string", api_id=API_ID, api_hash=API_HASH, in_memory=True)
 
 with client as app:
     str_session = app.export_session_string()
-    app.send_message(
-        "me",
-        f"#HU_STRING_SESSION\n\n```{str_session}```"
-    )
+    app.send_message("me", f"#HU_STRING_SESSION\n\n```{str_session}```")
     print(f"#HyperGenX String Session Has Been Generated :\n\n{str_session}\n\n It has been sent To Your Saved Messages as well.")
