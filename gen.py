b1e4bb5525ea53f3c863453714f376ca070e1f5b
anonymousx97 <88324835+anonymousx97@users.noreply.github.com>
2023-10-08 15:52:03 +0530

    Drop UX support, Fix monospace by using html tag instead of MD.

--- gen.py
+++ gen.py
@@ -1,36 +1,11 @@
-import subprocess
-
-print(
-    """Enter 1 if generating for Userge-X bot.
-Enter 2 for Pyrogram V2.
-*** DO NOT enter 2 if generating for Userge-X. ***
-"""
-)
-
-while True:
-    inp = input("> ")
-    if inp in [1, "1"]:
-        subprocess.call("python3 -m pip install pyrogram==1.4.7", shell=True)
-        break
-    elif inp in [2, "2"]:
-        subprocess.call("python3 -m pip install -U pyrogram", shell=True)
-        break
-    else:
-        print("Wrong Input\nTry Again.")
-
-subprocess.call("clear",shell=True)
-
-from pyrogram import Client, __version__
+from pyrogram import Client
 
 API_ID = int(input("Enter API_ID: "))
 API_HASH = input("Enter API_HASH: ")
 
-if __version__ == "1.4.7":
-    client = Client(session_name=":memory:", api_id=API_ID, api_hash=API_HASH)
-else:
-    client = Client(name="string", api_id=API_ID, api_hash=API_HASH, in_memory=True)
+client = Client(name="string", api_id=API_ID, api_hash=API_HASH, in_memory=True)
 
 with client as app:
     str_session = app.export_session_string()
-    app.send_message("me", f"#HU_STRING_SESSION\n\n```{str_session}```")
-    print(f"#HyperGenX String Session Has Been Generated :\n\n{str_session}\n\n It has been sent To Your Saved Messages as well.")
+    app.send_message("me", f"#SESSION_STRING\n\n<code>{str_session}</code>")
+    print(f"String Session Has Been Generated :\n\n{str_session}\n\n It has been sent To Your Saved Messages as well.")
